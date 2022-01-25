3c87ca1cef9f8443185b0d22e2c4198249f985c4
karan-2809 <7387168+karan-2809@users.noreply.github.com>
2022-01-25 18:15:46 +0530

    Update index.js
    typo fix

--- src/components/views/Dashboard/index.js
+++ src/components/views/Dashboard/index.js
@@ -14,27 +14,27 @@ import LineChart from '../../common/Graphs/index'
 
 const Dashboard = ({active}) => {
     let goals = []
-    const [dashboard,setDashboard] = useState('')
-    const [total,setTotal] = useState(0)
+    const [dashboard, setDashboard] = useState('')
+    const [total, setTotal] = useState(0)
     const [error, setError] = useState('');
 
     useEffect(() => {
-             getData() 
+        getData()
     }, []);
 
     const getData = () => {
         setError('');
-        getDashboardData().then(response=>{
+        getDashboardData().then(response => {
             goals = response.data.dashboard.goals
             var total = goals.map(goal => goal.count).reduce((acc, goal) => goal + acc);
-            setDashboard(response.data.dashboard)  
+            setDashboard(response.data.dashboard)
             setTotal(total)
-            console.log(response.data.dashboard.chart_data) 
+            console.log(response.data.dashboard.chart_data)
         }).catch(err => {
 
             setError(err.message);
         });
-          
+
     }
 
     return (
@@ -47,86 +47,83 @@ const Dashboard = ({active}) => {
                 <div className="container">
                     <div className="row">
 
-                        <SideBar active={active} />
+                        <SideBar active={active}/>
                         <div className="col-md-9">
-                        <h1 className="font_30"><i className="fa fa-home mr-2"></i>Dashboard</h1>
-        
-                        <div className="row mt-5">
-                        <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
-                                <div className="dashboard_div active">
-                                    <div className="row h-100">
-                                        <div className="col-8 pr-0 d-flex align-content-center">
-                                            <div>
-                                                <h1 className="mb-0">{total}</h1>
-                                                <p className="mb-0">Total Goals</p>
+                            <h1 className="font_30"><i className="fa fa-home mr-2"></i>Dashboard</h1>
+
+                            <div className="row mt-5">
+                                <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
+                                    <div className="dashboard_div active">
+                                        <div className="row h-100">
+                                            <div className="col-8 pr-0 d-flex align-content-center">
+                                                <div>
+                                                    <h1 className="mb-0">{total}</h1>
+                                                    <p className="mb-0">Total Goals</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="col-4 d-flex">
-                                        <i className="fa fa-bullseye mt-auto text-white font_30" aria-hidden="true"></i>
+                                            <div className="col-4 d-flex">
+                                                <i className="fa fa-bullseye mt-auto text-white font_30"
+                                                   aria-hidden="true"></i>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            { dashboard.goals && dashboard.goals.map((item,index)=>{ 
-                                
-                                return (
-                                    item._id!=null &&
-                                    <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
-                                        <div className="dashboard_div">
-                                            <div className="row h-100">
-                                                <div className="col-8 pr-0 d-flex align-content-center">
-                                                    <div>
-                                                        <h1 className="mb-0">{item.count}</h1>
-                                                        <p className="mb-0">{item._id} Goals</p>
+                                {dashboard.goals && dashboard.goals.map((item, index) => {
+
+                                    return (
+                                        item._id != null &&
+                                        <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
+                                            <div className="dashboard_div">
+                                                <div className="row h-100">
+                                                    <div className="col-8 pr-0 d-flex align-content-center">
+                                                        <div>
+                                                            <h1 className="mb-0">{item.count}</h1>
+                                                            <p className="mb-0">{item._id} Goals</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-4 d-flex">
+                                                        <i className="fa fa-clock mt-auto text-primary font_30"
+                                                           aria-hidden="true"></i>
                                                     </div>
                                                 </div>
-                                                <div className="col-4 d-flex">
-                                                <i className="fa fa-clock mt-auto text-primary font_30" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+
+                                    )
+
+                                })}
+
+                                {dashboard.salary &&
+                                <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
+                                    <div className="dashboard_div">
+                                        <div className="row h-100">
+                                            <div className="col-8 pr-0 d-flex align-content-center">
+                                                <div>
+                                                    <h1 className="mb-0">{dashboard.salary}</h1>
+                                                    <p className="mb-0">Income</p>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    
-                                )
-                                
-                            })}
-                            
-                            { dashboard.salary &&
-                            <div className="col-md-3 col-lg-3 col-6 mb-4 mb-lg-0">
-                                <div className="dashboard_div">
-                                    <div className="row h-100">
-                                        <div className="col-8 pr-0 d-flex align-content-center">
-                                            <div>
-                                                <h1 className="mb-0">{dashboard.salary}</h1>
-                                                <p className="mb-0">Income</p>
-                                            </div>
-                                        </div>
-                                        <div className="col-4 d-flex">
-                                        <i className="fa fa-dollar-sign mt-auto text-primary font_30"></i>
+                                            <div className="col-4 d-flex">
+                                                <i className="fa fa-dollar-sign mt-auto text-primary font_30"></i>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                }
                             </div>
-                            }
-                        </div>
-                        {dashboard.chart_data && 
+                            {dashboard.chart_data &&
                             <div className="col-md-9">
                                 <LineChart columns={dashboard.chart_data}/>
                             </div>
-                        }
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-
         </>
     );
 };
 
 export default Dashboard
-/*
-<div className="col-md-9">
-                            <PieChart columns={columns}/>
-                        </div>
-*/
